4d82b7bb821be133b4723382f34069af9915c3d9
Your Name <you@example.com>
2022-04-16 14:38:34 +0200

    hello

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+   /* Creates a nest application for monolithic servers */
+  const app = await NestFactory.create(AppModule, { logger: false });
+  
+  // validation is done through Nestjs pipes. 
+    // Nestjs Pipes run before a specified request (kinda like middlewre)
+    /* Enabling Validation pipe globally.
+       Thoough it can be done in module level too*/
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
